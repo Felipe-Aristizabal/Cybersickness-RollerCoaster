6cd9d486616bff54a499e8d4c8e6620ffa2eb160
Felipe <faristizabal00@gmail.com>
2024-05-21 21:13:23 -0500

    feat: Logic to reposition game objects for reduce FOV added

--- Assets/Scripts/ReduceFOVcontroller.cs
+++ Assets/Scripts/ReduceFOVcontroller.cs
@@ -10,14 +10,20 @@ namespace Rollercoaster
         private Rigidbody trainRigidBody;
         private CoasterTrain coasterTrainScript; 
         [SerializeField] Camera cameraVRplayer;
-        [SerializeField] GameObject canvasPlayer;
 
-        private GameObject bound1;
-        private GameObject bound2;
-        private GameObject bound3;
-        private GameObject bound4;
-        private float velocityToReduceFov = 1.0f;
-        private float scaleToReduce = 2.5F;
+        // Right Bound
+        [SerializeField] private GameObject bound1;
+        // Left Bound
+        [SerializeField] private GameObject bound2;
+        // Upper Bound
+        [SerializeField] private GameObject bound3;
+        // Bottom Bound
+        [SerializeField] private GameObject bound4;
+
+        private Transform previousTransformBound1;
+        private Transform previousTransformBound2;
+        private Transform previousTransformBound3;
+        private Transform previousTransformBound4;
 
         // Start is called before the first frame update
         void Start()
@@ -30,12 +36,10 @@ namespace Rollercoaster
                 coasterTrainScript = trainGameObject.GetComponent<CoasterTrain>();
             }
 
-            bound1 = canvasPlayer.transform.GetChild(0).gameObject;
-            bound2 = canvasPlayer.transform.GetChild(1).gameObject;
-            bound3 = canvasPlayer.transform.GetChild(2).gameObject;
-            bound4 = canvasPlayer.transform.GetChild(3).gameObject;
-
-            Debug.Log(bound3.gameObject.name);
+            previousTransformBound1 = bound1.transform;
+            previousTransformBound2 = bound2.transform;
+            previousTransformBound3 = bound3.transform;
+            previousTransformBound4 = bound4.transform;
         }
 
         // Update is called once per frame
@@ -44,27 +48,21 @@ namespace Rollercoaster
             //Debug.Log("My actual velocity is:" + coasterTrainScript.velocity);
             if (coasterTrainScript.velocity > 15)
             {
-                scaleToReduce = 2;
                 Debug.Log("Voy rapidisimo");
-                velocityToReduceFov = 1.0f;
- 
-                bound1.transform.localScale = new Vector3(scaleToReduce, scaleToReduce, scaleToReduce);
-                bound2.transform.localScale = new Vector3(scaleToReduce, scaleToReduce, scaleToReduce);
-                bound3.transform.localScale = new Vector3(scaleToReduce, scaleToReduce, scaleToReduce);
-                bound4.transform.localScale = new Vector3(scaleToReduce, scaleToReduce, scaleToReduce);
-
+                
+                bound1.transform.position = previousTransformBound1.position;
+                bound2.transform.position = previousTransformBound2.position;
+                bound3.transform.position = previousTransformBound3.position;
+                bound4.transform.position = previousTransformBound4.position;
             }
             else
             {
-                scaleToReduce = 1;
                 Debug.Log("Voy lentisimo");
-                velocityToReduceFov = -1.0f;
-
-                bound1.transform.localScale = new Vector3(scaleToReduce, scaleToReduce, scaleToReduce);
-                bound2.transform.localScale = new Vector3(scaleToReduce, scaleToReduce, scaleToReduce);
-                bound3.transform.localScale = new Vector3(scaleToReduce, scaleToReduce, scaleToReduce);
-                bound4.transform.localScale = new Vector3(scaleToReduce, scaleToReduce, scaleToReduce);
                    
+                bound1.transform.position = new Vector3(1.457f, bound1.transform.position.y, bound1.transform.position.z);
+                bound2.transform.position = new Vector3(-1.49f, bound2.transform.position.y, bound2.transform.position.z);
+                bound3.transform.position = new Vector3(bound3.transform.position.x, 1.357f, bound3.transform.position.z);
+                bound4.transform.position = new Vector3(bound4.transform.position.x, -1.513f, bound4.transform.position.z);
             }
         }
     }
